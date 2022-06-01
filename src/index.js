01dd19a719e0d099c3a29e71dd72b91f29ed4719
bamsi <hibamsi@gmail.com>
2022-06-01 12:02:00 +0300

    clearn dev

--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
-import React from "react"
+import React from "react";
 import ReactDOM from 'react-dom/client';
-import { Route, Routes } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom"
 
-import TodoContainer from "./components/TodoContainer";
-import About from "./pages/About";
-import NotMatch from "./pages/NotMatch";
-import Navbar from "./components/Navbar";
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
 
-import './App.css';
-
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container); 
-
-root.render(<React.StrictMode><BrowserRouter>
-<Navbar />
-
-<Routes>
-        <Route path="/" element={<TodoContainer />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="*" element={<NotMatch />}></Route>
-      </Routes>
-</BrowserRouter></React.StrictMode>);
-
+root.render(
+  <div>
+    <h1>Welcome to reach</h1>
+  </div>
+);
